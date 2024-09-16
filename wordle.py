5c9f017fa2f6d6d4c8beaa4e396d4200e181b0ff
megannn3 <possiblymegansinclair@gmail.com>
2024-09-16 18:25:27 +0000

    added wordle game

---
+++ wordle.py
@@ -0,0 +1,28 @@
+word = "great"
+default = '\033[0m'
+green = '\033[92m'
+yellow = '\033[33m'
+
+def generate_hint(user_guess):
+    color = default
+    hint = ""
+    for i in range(5):
+        if (user_guess[i] ==word[i]):
+            color = green
+        elif(user_guess[i] in word):
+            color= yellow
+        else:
+            color = default
+        hint = hint + color +user_guess[i]+ default
+    return hint
+
+
+def game_loop():
+    for i in range(6):
+        guess = input("What is your guess?")
+        print(generate_hint(guess))
+        if guess == word:
+            print("Congratulations!!!")
+            break
+
+game_loop()
